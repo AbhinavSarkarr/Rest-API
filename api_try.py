ffece064e83d6e262f873b8ff4ca42d41235cb46
Abhinav Sarkar <abhinavsarkar53@gmail.com>
2025-01-26 11:07:23 +0530

    chnages

--- api_try.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-response = requests.get("https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&site=stackoverflow")
-
-# print(response.json()['items'][0]['title'])
-
-for item in response.json()['items']:
-    if item['is_answered'] == False:
-        print(item['title'])
